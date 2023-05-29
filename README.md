0523a6e2432ddafcff193dec8634a6a8977454d9
codyprupp <crupp@ucsd.edu>
2023-05-28 23:48:30 -0700

    finished explore

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Lab 8 - Starter
 
+Partner Names: Cody Rupp
+
 1. Within a Github action that runs whenever code is pushed, because it guarantees that the code will be tested as it is updated, whereas running it locally is prone to human error.
 2. No
+3. No, because you would also want to verify that the other user has actually received the message, which you would likely not be able to do from a single "send" function.
+4. Yes, because such a feature does not really interact/depend on the other features of the application, and thus the function can be tested with unit tests.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,88 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+// isPhoneNumber
+test('000-000-0000 is valid phone number', () => {
+    expect(functions.isPhoneNumber('000-000-0000')).toBe(true);
+});
+
+test ('(123) 456-7890 is a valid phone number', () => {
+    expect(functions.isPhoneNumber('(123) 456-7890')).toBe(true);
+});
+
+test('0-0-0 is not a valid phone number', () => {
+    expect(functions.isPhoneNumber('0-0-0')).toBe(false);
+});
+
+test('(1234567890) is not a valid phone number', () => {
+    expect(functions.isPhoneNumber('(1234567890)')).toBe(false);
+});
+
+// isEmail
+test('crupp@ucsd.edu is a valid email', () => {
+    expect(functions.isEmail('crupp@ucsd.edu')).toBe(true);
+});
+
+test('hello@gmail.gov is a valid email', () => {
+    expect(functions.isEmail('hello@gmail.gov')).toBe(true);
+})
+
+test('asdf@ is not a valid email', () => {
+    expect(functions.isEmail('asdf@')).toBe(false);
+})
+
+test('cse110@cse110. is not a valid email', () => {
+    expect(functions.isEmail('cse110@cse110.')).toBe(false);
+})
+
+// isStrongPassword
+test('aA000 is a strong password', () => {
+    expect(functions.isStrongPassword('aA000')).toBe(true);
+})
+
+test('password is a strong password', () => {
+    expect(functions.isStrongPassword('password')).toBe(true);
+});
+
+test('asdofj1029j1092dj2019 is not a strong password', () => {
+    expect(functions.isStrongPassword('asdofj1029j1092dj2019')).toBe(false);
+});
+
+test('hi is not a strong password', () => {
+    expect(functions.isStrongPassword('hi')).toBe(false);
+});
+
+// isDate
+test('0/0/0000 is a valid date', () => {
+    expect(functions.isDate('0/0/0000')).toBe(true);
+});
+
+test('12/12/9999 is a valid date', () => {
+    expect(functions.isDate('12/12/9999')).toBe(true);
+});
+
+test('0/0/0 is not a valid date', () => {
+    expect(functions.isDate('0/0/0')).toBe(false);
+});
+
+test('0 is not a valid date', () => {
+    expect(functions.isDate('0')).toBe(false);
+});
+
+// isHexColor
+test('#FFFFFF is a valid hex color', () => {
+    expect(functions.isHexColor('#FFFFFF')).toBe(true);
+});
+
+test('0A0 is a valid hex color', () => {
+    expect(functions.isHexColor('0A0')).toBe(true);
+});
+
+test('##FFFFFF is not a valid hex color', () => {
+    expect(functions.isHexColor('##FFFFFF')).toBe(false);
+});
+
+test('0 is not a valid hex color', () => {
+    expect(functions.isHexColor('0')).toBe(false);
+});
